393ce02f98eb8e456d08b9766c36824f60e438c1
Mauro Erben <mauroerben100@hotmail.com>
2022-04-16 22:05:50 +0000

    Changes in navbar

--- src/Components/Header/navbar/menuItems.js
+++ src/Components/Header/navbar/menuItems.js
@@ -3,7 +3,7 @@ import { Link as ReachLink } from 'react-router-dom';
 
 export const MenuItems = ({ children, to = '/' }) => {
     return (
-        <Link _hover={{textDecoration: 'none'}} as={ReachLink} to={to}>
+        <Link as={ReachLink} to={to}>
             {children}
         </Link>
     )
